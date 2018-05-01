4cb82381d56c6fcb5743d1856cc35fc1d1684b30
Sergio Moya <seromoya15@hotmail.com>
2018-05-01 15:53:36 -0600

    no bin shaped data

--- src/tec/ic/ia/pc1/g08_data.py
+++ src/tec/ic/ia/pc1/g08_data.py
@@ -37,3 +37,33 @@ def shaped_data(n):
     return X,dummy_y
 
 
+def shaped_data_no_bin(n):
+    dataset = numpy.array(g08.generar_muestra_pais(n))
+
+
+    X = dataset[:,1:32].astype(float)
+    X0 = dataset[:,0]
+    X32 = dataset[:,32]
+    Y = dataset[:,33]
+
+    # encode class values as integers
+    encoderY = LabelEncoder()
+    encoderY.fit(Y)
+    encoded_Y = encoderY.transform(Y)
+
+    # encode class values as integers
+    encoderX0 = LabelEncoder()
+    encoderX0.fit(X0)
+    X0 = encoderX0.transform(X0)
+
+    # encode class values as integers
+    encoderX32 = LabelEncoder()
+    encoderX32.fit(X32)
+    X32 = encoderX32.transform(X32)
+
+    X = numpy.concatenate((X0.reshape((-1, 1)), X), axis=1)
+    X = numpy.concatenate((X, X32.reshape((-1, 1))), axis=1)
+
+    X = numpy.concatenate((X, Y.reshape((-1, 1))), axis=1)
+    
+    return X,dummy_y
